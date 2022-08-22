4c5a06c328cfa87965fadfbd729c320f0c5baaff
jjheffernan <jjheffernan99@gmail.com>
2022-08-22 00:57:55 -0400

    fixed range bug found in lotto.py

--- powerball.py
+++ powerball.py
@@ -8,11 +8,11 @@ from random import randint
 # picking a single lottery number
 def pick_lotto():
 
-    max_m = 53  # max number = m
-    max_j = 6
+    max_m = 69  # max number = m
+    max_j = 5
     m = max_m
     # create all numbers from 0 to m
-    r = range(m+1)
+    r = list(range(m+1))
     # start with an empty result
     v = []
     for j in range(max_j):
@@ -21,14 +21,40 @@ def pick_lotto():
         n = r[i]
         # remove it from r...
         r[i:i+1] = []
-        m = m-1
+        m = m - 1
         # and append to the result
         v.append(n)
         return v
 
 
+# picking the final number
+def pick_powerball():
+    max_m = 26  # max number = m
+    max_j = 1
+    m = max_m
+    # create all numbers from 0 to m
+    r = list(range(m + 1))
+    # start with an empty result
+    v = []
+    for j in range(max_j):
+        # get ith number from r...
+        i = randint(1, m)
+        n = r[i]
+        # remove it from r...
+        r[i:i + 1] = []
+        m = m - 1
+        # and append to the result
+        v.append(n)
+        return v
+    # pass
+
+
+
+
+
 def run():
-    done=0
+    done = 0
+
     while not done:
         try:
             x = input('\npress Enter for Lotto picks (Q to quit). ')
